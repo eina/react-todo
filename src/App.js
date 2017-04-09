d90e93690d1a5335b6c46a5c2cae87c7ad991f88
thelittleblacksmith <einers@outlook.com>
2017-04-09 12:37:57 -0700

    create AddToDo component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
+// import components
 import ToDoList from './components/ToDoList';
+import AddToDo from './components/AddToDo';
 
 
 class App extends Component {
@@ -24,12 +27,17 @@ class App extends Component {
     };
   }
 
+  handleAddToDo (text){
+    alert('new todo: ' + text)
+  }
+
   render() {
     let {todos} = this.state;
 
     return (
       <div className="App">
         <ToDoList todos={todos} />
+        <AddToDo onAddTodo={this.handleAddToDo} />
       </div>
     );
   }
---
+++ src/components/AddToDo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class AddToDo extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    let todoText = this.refs.todoText.value;
+
+    if(todoText.length > 0){
+      //clear out the value
+      this.refs.todoText.value = '';
+      //onAddTodo is a prop of this component 
+      //that runs the function (in App.js): handleAddToDo
+      this.props.onAddTodo(todoText);
+    } else {
+      //focus on the field so they can put data since it's empty
+      this.refs.todoText.focus();
+    }
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add to do item here"
+          ref="todoText" />
+        <button type="submit">Add To Do</button>
+      </form>
+    );
+  }
+}
