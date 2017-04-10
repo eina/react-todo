3326417b9494371c4020b16a9e44c4b3788c58ee
thelittleblacksmith <einers@outlook.com>
2017-04-09 18:17:20 -0700

    add todos reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,3 +1,6 @@
+import uuid from 'node-uuid';
+import moment from 'moment';
+
 let searchTextReducer = (state='', action) => {
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
@@ -16,8 +19,38 @@ let showCompletedReducer = (state=false, action) => {
   }
 }
 
+let todosReducer = (state = [], action) => {
+  switch(action.type){
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id){
+          let nextCompleted = !todo.completed;
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          }
+        }
+      })
+    default:
+      return state;
+  }
+}
 
 export {
   searchTextReducer,
-  showCompletedReducer
+  showCompletedReducer,
+  todosReducer
 }
