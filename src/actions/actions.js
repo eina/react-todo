aae62a84f0d213597e1917abd29090257773e72e
thelittleblacksmith <einers@outlook.com>
2017-04-09 17:52:45 -0700

    add showCompleted and searchText reducers

---
+++ src/actions/actions.js
@@ -0,0 +1,34 @@
+let setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  }
+}
+
+let addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  }
+}
+
+let toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED',
+  }
+}
+
+let toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
+
+
+export {
+  setSearchText,
+  addTodo,
+  toggleShowCompleted,
+  toggleTodo
+}
